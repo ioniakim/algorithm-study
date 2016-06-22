1147be7c3514798bee1f8bf721846af203e3c105
Sung Woo Kim <ionia.kim@gmail.com>
2016-06-22 23:40:35 +0900

    added reverse2

--- reverse.py
+++ reverse.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import math
 
+first = True
 
 def reverse(n):
+  global first
   if n < 10:
     print(n)
     return
 
-  print(n % 10, end='')
-  reverse(n//10)
+  m = n % 10
+  if first and m == 0:
+    reverse(n//10)
+  else:
+    first = False
+    print(n % 10, end='')
+    reverse(n//10)
+
+def reverse2(n):
+  if n < 10:
+    return n
+
+  return reverse2(n//10) + (n % 10) * 10 ** int(math.log10(n))
 
 def main():
-  reverse(100)
+  n = 100100013240
+  reverse(n)
+  print(reverse2(n))
 
 if __name__ == '__main__':
   main()
