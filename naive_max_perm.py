a03f26e0e2963c8991e2ae52d8939416542598a2
Sung Woo Kim <ionia.kim@gmail.com>
2016-10-03 21:24:31 +0900

    Add description of max_perm problem

--- naive_max_perm.py
+++ naive_max_perm.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+"""
+Finding a Maximum Permutation
+Eight persons with very particular tastes have bought tickets to movies. Some of them are happy with their seats, but most of them are not, adn after standing in line, they're getting a bit grumpy. Let's say each of them has favorite seat, and you want to fidn a way to let them switch seats to make as many people as possible happy with the result. However, because they are all rather grumpy, all of them refuse to move to another seat if they can't get their favorite.
+"""
 
 def naive_max_perm(M, A=None):
     if not A:
