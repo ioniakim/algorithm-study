a83a855e97d63b86fbe2ea3f8db6e367a252bdbd
Sung Woo Kim <ionia.kim@gmail.com>
2016-10-03 21:07:21 +0900

    Complete max_perm algorithm

--- max_perm.py
+++ max_perm.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 
 def max_perm(M):
-    n = len(M)
-    A = set(range(n))
-    pass
+    n = len(M)                      # How many elements?
+    A = set(range(n))               # A = {0, 1, ... , n-1}
+    count = [0 for _ in range(n)]    # C[i] == 0 for i in A
+    for i in M:                     # All that are pointed to
+        count[i] +=1                # Increment point count
+
+    Q = [i for i in A if count[i] == 0] # Useless elements
+    while Q:                            # While useless elts. left
+        i = Q.pop()                     # Get one
+        A.remove(i)                     # Remove it
+        j = M[i]                        # Who's it pointing to?
+        count[j] -= 1                   # Decrement point count
+        if count[j] == 0:               # Is j useless?
+            Q.append(j)                 # Then deal with it next
+
+    return A                            # Return useful elts.
 
 
 def main():
